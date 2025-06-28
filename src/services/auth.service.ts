c93d99e8c55b61cbe8943644deccb26523a84f1c
Korede Anunusor <koredeanunusor@gmail.com>
2025-06-28 21:59:29 +0100

    user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { SenderEmailModel } from "../models/SenderEmail";
+import { User } from "../models/User";
 
 export interface AuthService {
-    login(data: {email: String}): Promise<{ accessToken: string; refreshToken: string }>;
-    createSenderEmail(data: {sender: String, type: String, email: String}): Promise<SenderEmailModel>;
+    login(data: {email: string, password: string}): Promise<{ accessToken: string; refreshToken: string }>;
+    createSenderEmail(data: {firstName: string, lastName: string, email: string, password: string}): Promise<User>;
   }
