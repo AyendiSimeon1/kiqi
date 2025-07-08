b159ce866a2d13743152bd3e2dbb21c5a3f01289
Korede Anunusor <koredeanunusor@gmail.com>
2025-07-08 09:07:00 +0100

    authentication

--- src/middlewares/Auth.middlewares.ts
+++ src/middlewares/Auth.middlewares.ts
@@ -4,10 +4,17 @@ import { User } from '../models/User';
 import { authService } from '../services/User';
 import { ApiError } from '../utils/ApiError';
 import { asyncHandler } from '../utils/AsyncHandler';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 
+interface JwtPayload {
+    
+    userId: string;
+    email: string;
+  
+  }
 
 export interface AuthRequest extends Request {
-    user?: User;
+    user?: string;
 }
 
 export const verifyJWT = asyncHandler(async (req: any  , res: Response, next: NextFunction) => {
@@ -32,3 +39,43 @@ export const verifyJWT = asyncHandler(async (req: any  , res: Response, next: Ne
         throw new ApiError(401, error instanceof Error ? error.message : 'Invalid access token');
     }
 });
+
+export const isAuthenticated = async (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
+    try{
+    const authHeader = req?.headers["authorization"];
+  
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      res.status(401).json({
+        error: true,
+        message: 'Authorization token missing or malformed',
+      });
+    }
+  
+    const token = authHeader?.split(' ')[1];
+
+    if(!token){
+        res.status(StatusCodes.UNAUTHORIZED).json({
+            message: "Token is missing from authorization header",
+        })
+    }
+  
+       jwt.verify(token || "", process.env.JWT_SECRET || "", (err, decode) => {
+        if(err){
+            res.status(StatusCodes.FORBIDDEN).json({
+                message: "Invalid or expired token"
+            })
+            return;
+        }
+
+        const payload = decode as JwtPayload;
+        req.user = payload.userId;
+        next()
+      }) 
+  
+    } catch (err: any) {
+      res.status(401).json({
+        message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+        error: err.message
+      });
+    }
+  };
