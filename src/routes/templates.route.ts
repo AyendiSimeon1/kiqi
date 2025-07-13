9dd55f39d9a0917a1862ffd58ad4eff309b7a2a9
Korede Anunusor <koredeanunusor@gmail.com>
2025-07-13 06:01:53 +0100

    template routes

---
+++ src/routes/templates.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { isAuthenticated } from "../middlewares/Auth.middlewares";
+import { TemplatesController } from "../controllers/template.controller";
+
+const templateRouter = Router()
+const templateController = new TemplatesController
+
+templateRouter.post("/", isAuthenticated, templateController.createTemplate);
+templateRouter.get("/:id", isAuthenticated, templateController.getTemplateById);
+templateRouter.get("/", isAuthenticated, templateController.getAllTemplates);
+templateRouter.delete("/:id", isAuthenticated, templateController.deleteTemplates)
