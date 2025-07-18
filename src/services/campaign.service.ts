ae9f572bf36f2604df03eaec85d33f33eaaa4038
Korede Anunusor <koredeanunusor@gmail.com>
2025-07-18 13:52:22 +0100

    campaign service

--- src/services/campaign.service.ts
+++ src/services/campaign.service.ts
@@ -3,4 +3,10 @@ import { CampaignDoc } from "../models/Campaign";
 export interface CampaignService{
     createCampaign(data: {campaignName: String, subjectLine: String, campaignType: String}): Promise<CampaignDoc>;
     getAllCampaigns(): Promise<CampaignDoc[]>
+    getCampaignById(id: String): Promise<CampaignDoc>
+    updateCampaign(id: String): Promise<CampaignDoc>
+    deleteCampaign(id: String): Promise<void>
+    sendCampaign(): Promise<CampaignDoc>
+    scheduleCampaign(): Promise<CampaignDoc>
+    getCampaignDeliveryStatus(): Promise<CampaignDoc>
 }
