44510cdda2cddb3f9c130f23350d2c31f08345c0
Korede Anunusor <koredeanunusor@gmail.com>
2025-07-31 12:23:30 +0100

    key

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -11,7 +11,7 @@ export function generateAccessToken(id: string, senderEmail: String): string {
   }
   
   export function generateRefreshToken(id: string, senderEmail: String): string {
-    const secret = process.env.JWT_SECRET!;
+    const secret = process.env.REFRESH_TOKEN_SECRET!;
     const expiresIn = process.env.JWT_REFRESH_EXPIRES || "7d";
   
     return Jwt.sign({ id, email: senderEmail }, secret, {
